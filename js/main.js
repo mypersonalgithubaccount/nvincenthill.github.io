03a7f064a4bfa6df10398413caced69f1d92a4ce
nvincenthill <nvincenthill@gmail.com>
2017-12-31 23:16:43 -0800

    Add game of life

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // Plays webm video on hover
-$(".video-cover").hover( hoverVideo, hideVideo );
+
+/* $(".video-cover").hover( hoverVideo, hideVideo );
 
 function hoverVideo(e) {
     this.nextElementSibling.play();
@@ -8,4 +9,86 @@ function hoverVideo(e) {
 function hideVideo(e) {
     this.nextElementSibling.currentTime = 0;
     this.nextElementSibling.pause();
+} */
+
+// Daniel Shiffman
+// http://codingtra.in
+
+function make2DArray(cols, rows) {
+  let arr = new Array(cols);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = new Array(rows);
+  }
+  return arr;
+}
+
+let grid;
+let cols;
+let rows;
+let resolution = 5;
+
+function setup() {
+  createCanvas(600, 400);
+  cols = width / resolution;
+  rows = height / resolution;
+
+  grid = make2DArray(cols, rows);
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j] = floor(random(2));
+    }
+  }
+}
+
+function draw() {
+  background(0);
+
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let x = i * resolution;
+      let y = j * resolution;
+      if (grid[i][j] == 1) {
+        fill(255);
+        stroke(0);
+        rect(x, y, resolution - 1, resolution - 1);
+      }
+    }
+  }
+
+  let next = make2DArray(cols, rows);
+
+  // Compute next based on grid
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let state = grid[i][j];
+      // Count live neighbors!
+      let sum = 0;
+      let neighbors = countNeighbors(grid, i, j);
+
+      if (state == 0 && neighbors == 3) {
+        next[i][j] = 1;
+      } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
+        next[i][j] = 0;
+      } else {
+        next[i][j] = state;
+      }
+
+    }
+  }
+
+  grid = next;
+
+}
+
+function countNeighbors(grid, x, y) {
+  let sum = 0;
+  for (let i = -1; i < 2; i++) {
+    for (let j = -1; j < 2; j++) {
+      let col = (x + i + cols) % cols;
+      let row = (y + j + rows) % rows;
+      sum += grid[col][row];
+    }
+  }
+  sum -= grid[x][y];
+  return sum;
 }
