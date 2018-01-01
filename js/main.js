a54adc595e2b6bcd6fff534307c09a873790f561
nvincenthill <nvincenthill@gmail.com>
2018-01-01 14:12:57 -0800

    fix more issues

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 // Daniel Shiffman
 // http://codingtra.in
 
@@ -7,25 +6,17 @@ let cols;
 let rows;
 let resolution = 5;
 let canvas;
-//
-//function centerCanvas() {
-//  var canvasx = (windowWidth - width) / 2;
-//  var canvasy = (windowHeight - height) / 2;
-//  canvas.position(canvasx, canvasy);
-//}
 
 function setup() {
-
-var canvas = createCanvas(400, 400);
-canvas.parent('sketch-holder');
+    var canvas = createCanvas(400, 400);
+    canvas.parent('sketch-holder');
+    cols = width / resolution;
+    rows = height / resolution;
     
-  cols = width / resolution;
-  rows = height / resolution;
-
-  space = make2DArray(cols, rows);
-  for (let i = 0; i < cols; i++) {
-    for (let j = 0; j < rows; j++) {
-      space[i][j] = floor(random(2));
+    space = make2DArray(cols, rows);
+        for (let i = 0; i < cols; i++) {
+            for (let j = 0; j < rows; j++) {
+                space[i][j] = floor(random(2));
     }
   }
 }
@@ -38,23 +29,15 @@ function make2DArray(cols, rows) {
   return arr;
 }
 
-
 function draw() {
   background(0);
-    let r = 50;
-    strokeWeight(0);
-    col2 = map(mouseX,0,height,255,0);
-    ellipse(mouseX,mouseY,r,r);
-    col = map(mouseX,0,height,0,255);
-    r = map(mouseY,0,width,0,600);
-    
-    
+
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
       let x = i * resolution;
       let y = j * resolution;
       if (space[i][j] == 1) {
-        fill(255);
+        fill('yellow');
         stroke(0);
         rect(x, y, resolution - 1, resolution - 1);
       }
@@ -78,7 +61,6 @@ function draw() {
       } else {
         next[i][j] = state;
       }
-
     }
   }
 
